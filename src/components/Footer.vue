bc2df72b81ffcb85e7517a0312ed6288713ba222
AdyanMidzic08 <Adyan.Midzic08@gmail.com>
2026-03-04 23:25:45 +0100

    made Components

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+  const copyRight = ref(Date());
+</script>
+
+<template>
+
+  <footer>
+    <p>&copy;  {{copyRight}}</p>
+  </footer>
+
+</template>
+
+<style scoped>
+  footer {
+      position:fixed;
+      bottom:0;
+      padding-bottom:20px;
+  }
+
+  footer p {
+    color: blue;
+  }
+</style>
